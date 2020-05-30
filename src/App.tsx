0244e039f775521e8fed9214b3b35a094f805c2e
ankit <ankittomer89@gmail.com>
2020-05-30 14:00:36 +0530

    form app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/common/Header';
+import Multitab from './Components/common/Multi-tab';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Multitab />
+    </div>
+  );
+}
+
+export default App;
